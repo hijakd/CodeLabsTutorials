5bae1b21e05aff212f9c4945c3c8cacee4d899bf
hijakd <jharyd@gmail.com>
2025-09-02 14:24:20 +1000

    u4 path3 adaptive layouts - begin 2nd [build an adaptive layout] codelab

--- app/u4_path3_adaptive_layouts/src/main/java/com/gdd/u4_path3_adaptive_layouts/MainActivity.kt
+++ app/u4_path3_adaptive_layouts/src/main/java/com/gdd/u4_path3_adaptive_layouts/MainActivity.kt
@@ -41,10 +41,30 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ReplyAppPreview() {
     CodeLabsTutorialsTheme {
         Surface {
             ReplyApp(windowSize = WindowWidthSizeClass.Compact)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppMediumPreview() {
+    CodeLabsTutorialsTheme {
+        Surface {
+            ReplyApp(windowSize = WindowWidthSizeClass.Medium)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppExpandedPreview() {
+    CodeLabsTutorialsTheme {
+        Surface {
+            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
         }
     }
 }
